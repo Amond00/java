cd77d203964808de6215dcc6fdd2dd3e6e290260
Amond00 <danielhan0717@gmail.com>
2022-09-05 18:03:59 +0900

    java Oper

---
+++ ch01/src/sec1/test.java
@@ -0,0 +1,13 @@
+package sec1;
+
+public class test {
+	byte bytevalue = 10;
+	float floatvalue = 2.5F;
+	double doubleValue = 2.5F;
+	//int result = 5 + byteValue;
+	//float result = 5 + floatValue;
+	//double result = 5 + doubleValue;
+	
+	
+
+}
