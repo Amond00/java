f193dc10f451de0214df3da075fdc7cf1c09c407
Amond00 <danielhan0717@gmail.com>
2022-09-29 17:47:00 +0900

    java interface, anonymous instance and exception

--- ch06/src/sec4/MemberEx.java
+++ ch06/src/sec4/MemberEx.java
@@ -3,8 +3,7 @@ package sec4;
 public class MemberEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Member user1 = new Member("홍길동", "hong");
+		Member user2 = new Member("김자바", "kim");
 	}
-
 }
